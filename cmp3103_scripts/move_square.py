701e382c1f1fe249c538ff74a3e5f402541dd61e
jondave <jcox289@gmail.com>
2023-01-25 14:38:24 +0000

    First commit

---
+++ cmp3103_scripts/move_square.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from cmp3103_scripts.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'cmp3103_scripts'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kasm-user',
+    maintainer_email='jcox289@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'move_square = cmp3103_scripts.move_square:main'
+        ],
+    },
+)
